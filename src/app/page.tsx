ed371099bf50b5cec7ffbc7f45a98dc35b29dd38
sudhanshu tiwary <sudhanshukumar321456@gmail.com>
2024-12-12 02:45:31 +0530

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/signup" >Signup</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <Link href="/signup" className="rounded-md bg-slate-500 px-4 py-2 text-white hover:bg-gray-100 hover:text-black">Signup</Link>
     </div>
   );
 }
