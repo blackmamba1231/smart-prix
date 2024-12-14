280b5133276f148a24e303d2d75aaa86d7963204
sudhanshu tiwary <116373262+blackmamba1231@users.noreply.github.com>
2024-12-15 00:11:34 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEST_PUBLIC_BACKEND_URL: "https://nest-js-rouge.vercel.app/"
+    NEST_PUBLIC_BACKEND_URL: "https://nestjs-v1-d4qpw3ptf-blackmamba1231s-projects.vercel.app/"
   },
 };
 
