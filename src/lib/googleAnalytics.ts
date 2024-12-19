8b7c31663e28b2579017eede04f07b0bed72417b
sudhanshu tiwary <sudhanshukumar321456@gmail.com>
2024-12-19 20:27:20 +0530

    thiii

--- src/lib/googleAnalytics.ts
+++ src/lib/googleAnalytics.ts
@@ -1,4 +1,4 @@
-export const trackEvent = (action: string, params: Record<string, Number>) => {
+export const trackEvent = (action: string, params: Record<string, number>) => {
     if (typeof window !== "undefined" && window.gtag) {
       window.gtag("event", action, params);
     }
